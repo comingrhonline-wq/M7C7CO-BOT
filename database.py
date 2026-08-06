dbe3f793d8886a3b0769d8d015f07e707d39423b
comingrhonline-wq <comingrh.online@gmail.com>
2026-08-06 03:05:52 -0300

    database.py

--- database.py
+++ database.py
@@ -1,24 +1,32 @@
 import aiosqlite
 from datetime import datetime
 
+
 DB = "m7c7co.db"
 
 
 async def criar_banco():
+
     async with aiosqlite.connect(DB) as banco:
-        await banco.execute("""
-        CREATE TABLE IF NOT EXISTS historico (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            numero INTEGER,
-            data TEXT
+
+        await banco.execute(
+            """
+            CREATE TABLE IF NOT EXISTS historico (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                numero INTEGER,
+                data TEXT
+            )
+            """
         )
-        """)
 
         await banco.commit()
 
 
+
 async def salvar_numero(numero):
+
     async with aiosqlite.connect(DB) as banco:
+
         await banco.execute(
             """
             INSERT INTO historico(numero, data)
@@ -26,15 +34,20 @@ async def salvar_numero(numero):
             """,
             (
                 numero,
-                datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                datetime.now().strftime(
+                    "%d/%m/%Y %H:%M:%S"
+                )
             )
         )
 
         await banco.commit()
 
 
-async def pegar_historico(limite=20):
+
+async def pegar_historico(limite=50):
+
     async with aiosqlite.connect(DB) as banco:
+
         cursor = await banco.execute(
             """
             SELECT numero, data
@@ -48,3 +61,17 @@ async def pegar_historico(limite=20):
         dados = await cursor.fetchall()
 
         return dados
+
+
+
+async def resetar_dados():
+
+    async with aiosqlite.connect(DB) as banco:
+
+        await banco.execute(
+            """
+            DELETE FROM historico
+            """
+        )
+
+        await banco.commit()
